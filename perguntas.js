831706dc8d5ba61a6931d67df66546b2858ba874
bonettilais <lais.bonetti@escola.pr.gov.br>
2025-09-09 08:45:48 -0300

    09/09

---
+++ perguntas.js
@@ -0,0 +1,36 @@
+criaCartao(
+    'biologia',
+    'Normalmente, quantos litros de sangue uma pessoa tem? Em média, quantos são retirados numa doação de sangue?',
+    'Entre 4 a 6 litros. São retirados 450 mililitros.'
+)
+
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta'
+)
+
+criaCartao(
+    'história',
+    'Quem inventou a lâmpada?',
+    'Thomas Edison.'
+)
+
+criaCartao(
+    'biologia',
+    'Quantos ossos temos no nosso corpo?',
+    '206 ossos.'
+)
+
+criaCartao(
+    'história',
+    'Quem pintou Mona Lisa?',
+    'Leonardo da Vinci.'
+)
+
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta'
+)
+
