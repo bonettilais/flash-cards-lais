b903f881a47d9e72a583e83cf5a922245ba66a02
bonettilais <lais.bonetti@escola.pr.gov.br>
2025-09-16 09:22:04 -0300

    16/09

--- perguntas.js
+++ perguntas.js
@@ -4,10 +4,10 @@ criaCartao(
     'Entre 4 a 6 litros. São retirados 450 mililitros.'
 )
 
-criaCartao(
-    'categoria',
-    'pergunta',
-    'resposta'
+Cartao(
+    'Física',
+    'O que é mais pesado: 1 quilo de algodão ou 1 quilo de ferro?',
+    'Os dois pesam a mesma quantidade: 1 quilo.'
 )
 
 criaCartao(
@@ -29,8 +29,50 @@ criaCartao(
 )
 
 criaCartao(
-    'categoria',
-    'pergunta',
-    'resposta'
+    'Química',
+    'Qual o nome popular do cloreto de sódio?',
+    'Sal de cozinha'
+)
+
+Cartao(
+    'medicina',
+    'Qual a especialidade do otorrinolaringologista?',
+    'Tratamento das doenças relacionadas a nariz, ouvido e garganta.'
+)
+
+Cartao(
+    'botânica',
+    'Que fruto nasce da oliveira?',
+    'Azeitona'
+)
+
+Cartao(
+    'geografia',
+    'Em que país se localiza o Taj Mahal?',
+    'Índia'
+)
+
+Cartao(
+    'química',
+    'Qual destas substâncias faz parte da composição do vidro?',
+    'Areia'
+)
+
+Cartao(
+    'História da Cultura Pop',
+    ' Quem criou os personagens Mickey e Minnie Mouse?',
+    'Walt Disney.'
+)
+
+Cartao(
+    'Entretenimento',
+    ' Quantos são os personagens principais do seriado norte-americano Friends?',
+    'Seis.'
+)
+
+Cartao(
+    'Cultura Pop',
+    'Qual artista é conhecido como o Rei do Pop?',
+    'Michael Jackson'
 )
 
